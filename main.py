be06a17e32907993292596ed91a959adc8abf63f
Ilya Lemeshevskyi <mercy.200111@gmail.com>
2023-09-14 14:49:36 +0300

    Fixed functions:
    - 'is_palindrome'
    - 'get_longest_palindrome'
    - 'are_parentheses_balanced'
    
    Added file asd.py(for tests) to gitignore

--- main.py
+++ main.py
@@ -1,38 +1,43 @@
 """
 Sequence functions implementations
 """
-
+import string
 from typing import Union
 
 
 def is_palindrome(origin: Union[str, int], /) -> bool:
-    def flipped(value):
-        return value[::-1]
-
-    if flipped(str(origin)) == str(origin):
+    test = str(origin).lower()
+    test = test.replace(' ', '')
+    for i in test:
+        if i in string.punctuation:
+            test = test.replace(i, '')
+    if str(test)[::-1] == str(test):
         result = True
     else:
         result = False
-
     return result
 
 
-assert is_palindrome("aba") is True
-assert is_palindrome("abc") is False
+assert is_palindrome("palindrome") is False
+assert is_palindrome("tattarrattat") is True
+assert is_palindrome("malayalam") is True
 assert is_palindrome(12345) is False
 assert is_palindrome(12321) is True
+assert is_palindrome("Serhii") is False
+assert is_palindrome("Hannah") is True
+assert is_palindrome("This is some sentence") is False
+assert is_palindrome("Mr. Owl ate my metal worm") is True
+assert is_palindrome("Satire: Veritas") is True
+assert is_palindrome("Dammit I'm Mad") is True
 
 
 def get_longest_palindrome(origin: str, /) -> str:
     res = []
 
-    def flipped(value):
-        return value[::-1]
-
     for i in range(len(origin)):
         for j in range(i + 1, len(origin) + 1):
             part = origin[i: j]
-            if flipped(part) == part:
+            if part[::-1] == part:
                 res.append(part)
 
     return max(res, key=len)
@@ -43,14 +48,22 @@ assert get_longest_palindrome("1012210") == "012210"
 
 
 def are_parentheses_balanced(origin: str, /) -> bool:
-    opened_pattern = ['(', '{', '[']
-    closed_pattern = [')', '}', ']']
+    opened_pattern = ['(', '{', '[', '<']
+    closed_pattern = [')', '}', ']', '>']
     result = 0
+    op_index = 0
+    cp_index = 0
     for i in origin:
+        if 0 != cp_index < op_index != 0:
+            return False
         if i in opened_pattern:
             result += 1
+            op_index = origin.index(i)
         elif i in closed_pattern:
             result -= 1
+            cp_index = origin.index(i)
+        elif i not in opened_pattern or closed_pattern:
+            continue
         else:
             return False
 
@@ -58,11 +71,16 @@ def are_parentheses_balanced(origin: str, /) -> bool:
         return False
     if result > 0:
         return False
+
     return True
 
 
+assert are_parentheses_balanced("(some <text>)") is True
+assert are_parentheses_balanced("") is True
+assert are_parentheses_balanced("[(<)}{(>)]") is False
+assert are_parentheses_balanced("[]]") is False
+assert are_parentheses_balanced("(()") is False
 assert are_parentheses_balanced("({[]})") is True
-assert are_parentheses_balanced(")]}{[(") is False
 
 
 def get_longest_uniq_length(origin: str, /) -> int:
@@ -78,10 +96,10 @@ def get_longest_uniq_length(origin: str, /) -> int:
         else:
             count = 0
             continue
-
     return max(l)
 
 
 assert get_longest_uniq_length("abcalkfgh") == 5
 assert get_longest_uniq_length("abcdefg") == 7
 assert get_longest_uniq_length("racecar") == 4
+assert get_longest_uniq_length("hwccjayhiszbmomlqkem") == 11
